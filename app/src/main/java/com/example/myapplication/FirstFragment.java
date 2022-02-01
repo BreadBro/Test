3283a03494bc380b39273b91df5409716a3a3017
BreadBro <laughinsafin@gmail.com>
2022-01-31 21:51:49 -0600

    ree

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,66 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.myapplication.databinding.FragmentFirstBinding;
+
+public class FirstFragment extends Fragment {
+    TextView showCountTextView;
+    int count;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View fragmentFirstLayout = inflater.inflate(R.layout.fragment_first, container, false);
+        showCountTextView = fragmentFirstLayout.findViewById(R.id.textview_first);
+        return fragmentFirstLayout;
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.toast_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast myToast = Toast.makeText(getActivity(), "Hello Toast!", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+
+        view.findViewById(R.id.count_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                countMe(view);
+            }
+        });
+    }
+    private void countMe(View view) {
+        String countString = showCountTextView.getText().toString();
+        count = Integer.parseInt(countString);
+        count++;
+        showCountTextView.setText(Integer.toString(count));
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        showCountTextView = null;
+    }
+
+}
